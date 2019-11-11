1a0a2971efc1c2a0c08fb9a33e24f218aea8345c
heiyanheimao <550715038@qq.com>
2019-11-12 02:29:48 +0800

    首页导航以及请求封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>首页</div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
 export default {
-
+  components: {
+    NavBar
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+  .home-nav {
+    background-color:var(--color-tint);
+    color: #fff;
+  }
 </style>
